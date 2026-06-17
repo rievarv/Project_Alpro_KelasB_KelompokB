1f4abae9dfc1f1b27815c0af1b85e45f85115398
RGhafiqi08 <rifqirifaghafiqi@gmail.com>
2026-06-17 21:58:50 +0700

    Update 03_akar_kuadrat.R

--- src_R/03_akar_kuadrat.R
+++ src_R/03_akar_kuadrat.R
@@ -1,1 +1,15 @@
+a <- 2
+b <- 4
+c <-5
 
+D <- b^2 - 4*a*c
+
+if (D < 0) {
+  cat("Akar imajiner\n")
+} else {
+  x1 <- (-b + sqrt(D)) / (2*a)
+  x2 <- (-b - sqrt(D)) / (2*a)
+  
+  cat(sprintf("Akar 1 = %.3f\n", x1))
+  cat(sprintf("Akar 2 = %.3f\n", x2))
+}
