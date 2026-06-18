4440801544601e7c8f56b5078fef13abefb2bf3b
rievarv <muhammad.ahyana@mhs.unsoed.ac.id>
2026-06-18 10:49:36 +0700

    Update 02_string_kalimat.R

--- src_R/02_string_kalimat.R
+++ src_R/02_string_kalimat.R
@@ -1,1 +1,16 @@
+# SOAL 2
+# Menggunakan tanda kutip tunggal atau ganda untuk membungkus
+K1 <- "Saya tak 'kan menyerah."
+K2 <- 'Ia berkata, "Aku menyayangimu."'
 
+# Menggunakan backslash untuk escape character pada kutip
+K3 <- '"Coba jelaskan pengertian \'cross-validation\' dalam Machine Learning!"'
+
+# Penulisan Standar
+K4 <- "Surat keputusan itu bernomor 62/UN.34/19/2023."
+
+# Tampilkan teks
+cat(K1, "\n")
+cat(K2, "\n")
+cat(K3, "\n")
+cat(K4, "\n")
