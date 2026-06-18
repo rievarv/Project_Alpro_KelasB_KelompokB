10141cb0ce3c7c8c66c53ab937fbdc79630e359a
rievarv <muhammad.ahyana@mhs.unsoed.ac.id>
2026-06-18 10:46:21 +0700

    Update 02_string_kalimat.py

--- src_python/02_string_kalimat.py
+++ src_python/02_string_kalimat.py
@@ -1,1 +1,18 @@
+# SOAL 2: Cetak String
+#Menggunakan kutip ganda sebagai pembungkus
+K1 = "Saya tak 'kan menyerah."
 
+#Menggunakan kutip tunggal sebagai pembungkus
+K2 = 'Ia berkata, "Aku menyayangimu."'
+
+# Menggunakan backslash (\) sebagai escape character untuk kutip di dalam string
+K3 = '"Coba jelaskan pengertian \'cross-validation\' dalam Machine Learning!"'
+
+# Penulisan standar
+K4 = "Surat keputusan itu bernomor 62/UN.34/19/2023."
+
+# Tampilkan teks
+print(K1)
+print(K2)
+print(K3)
+print(K4)
