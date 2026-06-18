113858c12bda13ebe35144aa99cbbbf89672faac
rievarv <muhammad.ahyana@mhs.unsoed.ac.id>
2026-06-18 09:03:51 +0700

    Update 06_interval_proporsi.R

--- src_R/06_interval_proporsi.R
+++ src_R/06_interval_proporsi.R
@@ -1,1 +1,33 @@
+proporsi<-0.7
+alpha<-10
+n<-15
 
+Interval <- function(proporsi, alpha, n) {
+  if (proporsi < 0 || proporsi > 1) {
+    return("Error: Proporsi sampel > 1 / < 0")
+  }
+  
+  if (alpha == 10) {
+    z <- 1.645
+  } else if (alpha == 5) {
+    z <- 1.96
+  } else {
+    return("Error: alpha harus 5 atau 10")
+  }
+  
+  margin <- z * sqrt((proporsi * (1 - proporsi)) / n)
+  batas_bawah <- proporsi - margin
+  batas_atas <- proporsi + margin
+  
+  return(list(bawah = batas_bawah, atas = batas_atas)) 
+}
+
+hasilnya <- Interval(proporsi,alpha,n)
+if (is.character(hasilnya)) {
+  cat(hasilnya, "\n")
+  } else {
+  cat("Batas Bawah :", hasilnya$bawah, "\n")
+  cat("Batas Atas  :", hasilnya$atas, "\n")
+  }
+}
+  
